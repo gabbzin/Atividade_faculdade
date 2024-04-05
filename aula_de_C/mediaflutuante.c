89c81c3336220b635f9171ca926a91661ac6e1e6
gabbzin <cgabrielpimentacruz111@gmail.com>
2024-04-05 18:06:46 -0300

    Exercicio de média com números flutuantes ou quebrados

--- aula_de_C/mediaflutuante.c
+++ aula_de_C/mediaflutuante.c
@@ -1,1 +1,25 @@
-#include 
+#include <stdio.h>
+
+int main(){
+
+    printf("\n\nPor favor, utlize o (.) em vez de virgula\n\n");
+    
+    float n1;
+    float n2;
+    float n3;
+
+    printf("Digite um numero nao inteiro: ");
+    scanf("%f", &n1);
+
+    printf("Digite outro numero nao inteiro: ");
+    scanf("%f", &n2);
+
+    printf("Digite o ultimo numero nao inteiro: ");
+    scanf("%f", &n3);
+
+    float media = (n1 + n2 + n3) / 3;
+
+    printf("\n\nA media dos numeros digitados e igual a %.2f", media);
+
+    return 0;
+}
