14ed4d57cb95ee88c6fd6b4d494fab764b882f0c
gabbzin <cgabrielpimentacruz111@gmail.com>
2024-04-05 17:57:35 -0300

    Finalização
    Código funcionando com sucesso

--- aula_de_C/contador.c
+++ aula_de_C/contador.c
@@ -3,29 +3,28 @@
 int main(){
     // Declaração das variáveis
     int segundos;
-    int minutos;
-    int horas;
+    int minutos = 0;
+    int horas = 0;
 
     printf("Digite o tempo em segundos: ");
-    scanf("%d", segundos);
+    scanf("%d", &segundos);
+
+    int segundoInformado = segundos;
 
     while (segundos > 60) { // Laço de repetição para realizar cálculos repetidos
         // Transformar os segundos em hora
         if (segundos >= 3600) {
-            segundos -= 3600;
             horas += 1;
+            segundos -= 3600;
         } else if (segundos >= 60) {
-            segundos -= 60;
             minutos += 1;
-        } else {
-            break;
+            segundos -= 60;
         }
     }
 
     // Mostrando o resultado
-
-    printf("Fiz a transformacao: \n");
-    printf("O tempo de %d segundos convertido no tempo equivalente a horas, minutos e segundos\n", segundos);
+    printf("\n\nFiz a transformacao: \n\n");
+    printf("O tempo de %d segundos convertido no tempo equivalente a horas, minutos e segundos\n", segundoInformado);
     printf("E igual a: %d horas %d minutos %d segundos.", horas, minutos, segundos);
 
     printf("\n\n==== Codigo executado com sucesso ====\n\n");
