f58eb45d83b4e16af0ce0f6096e4ad1b5259a90d
Gabriel P. <137789657+gabbzin@users.noreply.github.com>
2024-11-18 08:52:20 -0300

    Adição das atividades do dia 18/11
    Realizar os testes dos códigos e mandar print

---
+++ Arquivos Java/1B/java/Carro/CarroTeste.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        Carro carro1 = new Carro();
+        carro1.marca = "Toyota";
+        carro1.modelo = "Corolla";
+        carro1.ano = 2020;
+        carro1.acelerar();
+    }
+}
