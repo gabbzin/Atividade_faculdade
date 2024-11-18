177d474865de2937805b05c91d0f0e9262b7014c
Gabriel P. <137789657+gabbzin@users.noreply.github.com>
2024-11-18 08:25:15 -0300

    Adição das atividades feitas em sala
    Os arquivos que foram upados, devem ser testados no Vscode, tirar print do resultado e enviar ao professor na plataforma

---
+++ Arquivos Java/1B/java/questao08.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class AreaRetangulo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite a largura do retângulo: ");
+        double largura = scanner.nextDouble();
+        System.out.print("Digite a altura do retângulo: ");
+        double altura = scanner.nextDouble();
+        double area = largura * altura;
+        System.out.println("A área do retângulo é: " + area);
+    }
+}
